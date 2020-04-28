d5157616143528f84ce5e805ce639710897cf296
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-28 09:16:00 +0100

    Updates testing routines

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -1,7 +1,32 @@
 import pytest
-from src.exercise import main
+import src.exercise
 
-def test_exercise(capsys):
-    main()
-    out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+def test_exercise():
+    input_values = ["Bridgend","Bridgend","Pencoed","Bridgend"]
+    input_values_stored = ["Bridgend","Bridgend","Pencoed","Bridgend"]
+    output = []
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    src.exercise.input = mock_input
+    src.exercise.print = lambda s : output.append(s)
+
+    src.exercise.main()
+
+    assert [output[0],output[1],output[2]] == ["Enter the first string:",\
+                                               "Enter the second string:",\
+                                               "Same"]
+   assert [output[3],output[4],output[5]] == ["Enter the first string:",\
+                                              "Enter the second string:",\
+                                              "Different"]
